f01dc4339333e9bcf1e1c304e75e0061f2e6ed56
edjunior3 <felic5850@gmail.com>
2021-10-01 21:50:01 -0300

    Código comentado2

--- CafeAppLis/js/dataset.js
+++ CafeAppLis/js/dataset.js
@@ -1,6 +1,6 @@
 const itens = [
   {
-  //pares "chave :  valor" separados por virgula do objeto json "itens"
+  //pares "chave :  valor" separados por virgula, nos objetos json presentes em "itens"
     "imagem":"https://images.unsplash.com/flagged/photo-1552938218-ac9ec40c702a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80",
     "nome":"Café",
     "preparo":"Lorem ipsum dolor sit amet, consectetur adipisicing elit."
